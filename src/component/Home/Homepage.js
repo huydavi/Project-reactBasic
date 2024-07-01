7061b8733841cbf66b544a311b9d736f19ccee39
huydavi <huyyduc1105@gmail.com>
2024-07-02 04:24:54 +0700

    update

--- src/component/Home/Homepage.js
+++ src/component/Home/Homepage.js
@@ -1,10 +1,20 @@
 import videoHomepage from "../../assets/video-homepage.mp4";
 const Homepage = (props) => {
   return (
-    <div>
+    <div className="homepage-container">
       <video autoPlay muted loop>
         <source src={videoHomepage} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="title-1">Make forms worth filling out</div>
+        <div className="title-2">
+          Get more data—like signups, feedback, and anything else—with forms
+          designed to be refreshingly different.
+        </div>
+        <div className="title-3">
+          <button>Get started-it's free</button>
+        </div>
+      </div>
     </div>
   );
 };
