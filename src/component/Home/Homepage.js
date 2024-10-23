933c3296fc68d5494c333e163f13cf2552489a47
huydavi <huyyduc1105@gmail.com>
2024-10-23 11:46:54 +0700

    update

--- src/component/Home/Homepage.js
+++ src/component/Home/Homepage.js
@@ -14,8 +14,11 @@ const Homepage = (props) => {
         <source src={videoHomepage} type="video/mp4" />
       </video>
       <div className="homepage-content">
-        <div className="title-1">{t("homepage-title1")}</div>
-        <div className="title-2">{t("homepage-title2")}</div>
+        <div className="title-1">There's a better way to ask</div>
+        <div className="title-2">
+          You don't want to make a boring form. Maybe it's time to give a try
+          us...
+        </div>
         <div className="title-3">
           {isAuthenticated === false ? (
             <button onClick={() => navigate("/login")}>
